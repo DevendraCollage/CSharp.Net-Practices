9dbabe386977ea3d3b89a3d87dd53e646204f7b2
Devendra Parmar <devendraparmarcollage@gmail.com>
2024-05-17 17:07:13 +0530

    First Two Programs of the CSharp

---
+++ HelloWorld.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * Author - Devendra Parmar
+ * Program - Write a C# Sharp program to print Hello and your name in a separate line.
+ */
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello"); // This line output Hello on the console
+            Console.WriteLine("Devendra Parmar"); // This line will print Devendra Parmar on the console in the next new line
+        }
+    }
+}
---
+++ SumTwoNum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Author: Devendra Parmar
+ * Program: Write a C# Sharp program to print the sum of two numbers.
+ */
+
+namespace SumTwoNum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The sum of 10 + 20 = " + (10+20)); // This line will print the sum of the two numbers
+        }
+    }
+}
