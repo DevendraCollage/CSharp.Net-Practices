b37b7c0a8e3cfffcab444bc6ce908bed0e9db3dc
Devendra Parmar <devendraparmarcollage@gmail.com>
2024-05-20 17:28:53 +0530

    Solve the two questions of the C#.Net 1. Arithmetic Operations user enter two number and perform some required arithmetic operations 2. User enter number multiplication table to print on the console

---
+++ ArithmaticOperations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+/*
+ * Author - Devendra Parmar
+ * Program - Write a C# Sharp program to print on screen the output of adding, subtracting, multiplying and dividing two numbers entered by the user.
+ */
+
+namespace ArithmaticOperations
+{
+   internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get the two number from the user
+            Console.Write("Input the first number : ");
+            int num1 = Convert.ToInt32(Console.ReadLine
+                ());
+            Console.Write("Input the second number : ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            // Perform the required arithmatic operations and print the result
+            Console.WriteLine(num1 + " + " + num2 + " = " + (num1 + num2));
+            Console.WriteLine(num1 + " - " + num2 + " = " + (num1 - num2));
+            Console.WriteLine(num1 + " x " + num2 + " = " + (num1 * num2));
+            Console.WriteLine(num1 + " / " + num2 + " = " + (num1 / num2));
+            Console.WriteLine(num1 + " mod " + num2 + " = " + (num1 % num2));
+            
+            
+        }
+    }
+}
---
+++ MultiplicationTable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+/*
+ * Author - Devendra Parmar
+ * Program - Write a C# Sharp program that prints the multiplication table of a number as input.
+ */
+
+namespace MultiplicationTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get the number from the user as user wants the multiplication table
+            Console.Write("Enter the number here : ");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            // Print the multiplication table
+            Console.WriteLine("The Multiplication table of : " + userNum);
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(userNum + " x " + i + " = " + (userNum * i));
+            }
+        }
+    }
+}
