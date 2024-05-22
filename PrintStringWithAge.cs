73e87812242469272887e5d3da9421a5b74ec4bc
Devendra Parmar <devendraparmarcollage@gmail.com>
2024-05-22 13:05:04 +0530

    Solve two question of the C#.Net 1. Print the string with give age by the user 2. Print the user entered number four times with the given pattern

---
+++ PrintStringWithAge.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+/*
+ * Author - Devendra Parmar
+ * Program - Write a C# Sharp program that takes an age (for example 20) as input and prints something like "You look older than 20".
+ */
+
+namespace PrintStringWithAge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get an age from the user
+            Console.Write("Enter Your Age Here - ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            // Concate string with age
+            string userStr = "You look older than " + age;
+
+            // Print the output to the user
+            Console.WriteLine(userStr);
+        }
+    }
+}
