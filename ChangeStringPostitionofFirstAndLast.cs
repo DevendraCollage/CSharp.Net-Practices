630638977eac4d9ee07db06e9f894b5b75fc6aa8
Devendra Parmar <devendraparmarcollage@gmail.com>
2024-05-24 10:47:07 +0530

    Solve the two questions of the C#.Net 1. Remove the specified character by the user from the give string 2. Change the position of the string first and last characters

---
+++ ChangeStringPostitionofFirstAndLast.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+/*
+ * Author - Devendra Parmar
+ * Program - Create a new string from a given string where the first and last characters will change their positions
+ */
+
+namespace ChangeStringPosition
+{
+    internal class Program
+    {
+        // This is the main method where the program execution starts
+        static void Main(string[] args)
+        {
+            // Displaying the result after the rearanging the string
+            Console.WriteLine(First_Last("Devendra Parmar")); // This will change the first and last characters of the string
+        }
+
+        // Function to rearrange the string first and last characters
+        public static string First_Last(string str)
+        {
+            return str.Length > 1 ? str.Substring(str.Length - 1) + str.Substring(1, str.Length - 2) + str.Substring(0, 1) : str;
+            // Explanation of the give program:
+            /*
+             * - First if the length of the string is greater than 1 then print the substring
+             * - If length of the string is not greater than 1 means string only have one character then print the whole string
+             */
+        }
+    }
+}
---
+++ RemoveSpecifiedChacracters.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+/*
+ * Author - Devendra Parmar
+ * Program - Remove specified a character from a non-empty string using index of a character
+ */
+
+namespace RemoveSpecifiedCharacters
+{
+    internal class Program
+    {
+        // This is the main method where the program execution starts
+        static void Main(string[] args)
+        {
+            // Displaying a result after removing the character at the specified index
+            // Index starts from the 0
+            Console.WriteLine(RemoveChar("DevendraParmar", 0));
+            Console.WriteLine(RemoveChar("Darshan University", 5));
+            Console.WriteLine(RemoveChar("India is the great the country!", 8));
+        }
+
+        // Function to remove character at the specified index
+        public static string RemoveChar(string str, int n)
+        {
+            return str.Remove(n, 1);
+            // Exaplnation of the Function Remove():
+            /*
+             * - in the remove function n means where the deletion starts.
+             * - 1 means how many characters to remove from the string.
+             */
+        }
+    }
+}
