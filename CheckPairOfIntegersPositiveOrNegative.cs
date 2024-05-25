127244fb333262d127e950ee791e5e6f471eaf21
Devendra Parmar <devendraparmarcollage@gmail.com>
2024-05-25 19:03:56 +0530

    Solve the questions of C#.net 1. Add the first character of the string to the first and last and return the string to the output 2. Check given pair of two integers are one negative and one is positive then return true otherwise return message

---
+++ CheckPairOfIntegersPositiveOrNegative.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+/*
+ * Author - Devendra Parmar
+ * Program - Write a C# program to check a pair of integers and return true if one is negative and one is positive.
+ */
+
+namespace CheckPairOfIntegersPositiveOrNegative
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get the Two Integers from the user
+            Console.Write("Input First Integer Number : ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Second Integer Number : ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            // Check the two integers
+            if(num1<0 && num2>0) // If this condition will match then inside this block will execute
+            {
+                Console.WriteLine("True");
+            }else // If above condition is not match then this will inside else block will execute
+            {
+                Console.WriteLine("Re-enter the numbers");
+            }
+        }
+    }
+}
---
+++ StrFirstCharacterAddFirstAndLast.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+/*
+ * Author - Devendra Parmar
+ * Program - Create a new string from a given string with the first character added at the front and back
+ */
+
+namespace StrFirstCharacterAddFirstAndLast
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get the string from the user
+            Console.Write("Enter your string here : ");
+            string str = Console.ReadLine();
+
+            // Extract the first character of the string and store it in the string variable
+            string firstChar = str.Substring(0, 1); // In This 0 means start index and 1 means of length 
+
+            // Now Append the string with this character first and last
+            string appendString = firstChar + " " + str + " " + firstChar; // In This line we are add the first character of the string to the first and last of the userstring
+            Console.WriteLine(appendString);
+        }
+    }
+}
