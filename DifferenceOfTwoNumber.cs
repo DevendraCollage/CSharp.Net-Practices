99b4beb2df731329483ffa06fa639d5588752c62
Devendra Parmar <devendraparmarcollage@gmail.com>
2024-05-26 15:06:29 +0530

    Solve the two question of the C#.Net 1. Sum of the two number adding some additional condition 2. Give the difference between to the two numbers

---
+++ DifferenceOfTwoNumber.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+/*
+ * Author - Devendra Parmar
+ * Program - Write a C# program to get the absolute value of the difference between two 
+ * given numbers. Return double the absolute value of the difference if the first number 
+ * is greater than the second number.
+ */
+
+namespace DifferenceOfTwoNumber
+{
+    internal class Program
+    {
+        // Function to return the difference between the two number
+        static int differenceNum(int n1, int n2)
+        {
+            if(n1>n2) // This is the condition if the first number will greater than second number then double the difference
+            {
+                int differ = n1 - n2;
+                return differ * differ;
+            }
+            else // Otherwise return the difference of the give number
+            {
+                return n1 - n2;
+            }
+        }
+        static void Main(string[] args)
+        {
+            // Get the two numbers from the user
+            Console.Write("Enter the number one here : ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the number second here : ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            // Call the function and pass the value and print the result
+            Console.WriteLine(differenceNum(num1, num2));
+        }
+    }
+}
---
+++ SumOfTwoNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+/*
+ * Author - Devendra Parmar
+ * Program - Compute the sum of two given integers, if two values are equal then return the triple of their sum
+ */
+
+namespace SumOfTwoNumber
+{
+    internal class Program
+    {
+        // Make an function to calculate the sum of the two integers
+        static int calculateSum(int n1, int n2)
+        {
+            if (n1 == n2)
+            {
+                return n1 + n2;
+            }
+            else
+            {
+                return (n1 + n2) * 3;
+            }
+        }
+        static void Main(string[] args)
+        {
+            // Get the two integers from the user
+            Console.Write("Enter the number one here : ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the number two here : ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            // Call the function and pass the number to the function
+            Console.WriteLine(calculateSum(num1, num2));
+        }
+    }
+}
