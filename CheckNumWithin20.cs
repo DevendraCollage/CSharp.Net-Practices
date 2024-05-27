3b6914cc7ae80cb281d272d725ed059c525fc749
Devendra Parmar <devendraparmarcollage@gmail.com>
2024-05-27 14:41:29 +0530

    Solve the two questions of the C#.Net 1. Check the sum of the given two integer is 20 or anyone of the integer is 20 2. Check the number withing 20 after that check with 100 or 200

---
+++ CheckNumWithin20.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+/*
+ * Author - Devendra Parmar
+ * Program -  Write a C# program to check if the given integer is within 20 of 100 or 200.
+ */
+
+namespace CheckNumWithin20
+{
+    internal class Program
+    {
+        // Funtion to check the given integer is within 20 of 100 or 200.
+        static bool checkInt(int n)
+        {
+            if((n-100) <= 20 || (n-200) <= 20)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            // Get the integer number from the user
+            Console.Write("Enter the number one here : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            // Call the function and print the result
+            Console.WriteLine(checkInt(num));
+        }
+    }
+}
---
+++ CheckTheSumis20.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+/*
+ * Author - Devendra Parmar
+ * Program - Write a C# program to check the sum of the two given integers. Return true if 
+ * one of the integers is 20 or if their sum is 20.
+ */
+
+namespace CheckTheSumis20
+{
+    internal class Program
+    {
+        /* Function to check the give two integers of the sum is 20 or given the number is
+         * one of the integer is true then retun true otherwise false.
+        */
+        static bool checkSum(int n1, int n2)
+        {
+            if((n1+n2) == 20 || n1 == 20 || n2 == 20)
+            {
+                Console.WriteLine("The given integer sum is 20 or one of the integer is 20 : ");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("The given integer sum is not 20 or one of the any number is not 20 : ");
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            // Get the two integers from the user
+            Console.Write("Enter the number one here : ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the number two here : ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            // Call the function and print the value
+            Console.WriteLine(checkSum(num1, num2));
+        }
+    }
+}
