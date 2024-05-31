90fa600aad421ea23c7ebbd0a1be33df224d4246
Devendra Parmar <devendraparmarcollage@gmail.com>
2024-05-31 12:35:47 +0530

    Solve the two questions of the C#.Net 1. File the size of the files given that in the byte 2. Convert the hexadecimal number to decimal number given by the user

---
+++ FindSizeOfFile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO; // Importing the System.IO namespace for file handling.
+
+/*
+ * Author - Devendra Parmar
+ * Program - Find the size of a specified file in bytes
+ */
+
+namespace FindSizeOfFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // You have to handle the Exception using try and catch
+            try
+            {
+                // Create the instance of the FileInfo to use the function to get the file info
+                FileInfo fi = new FileInfo("C:\\Users\\Devendra\\Desktop\\Demo.txt"); 
+
+                // Print the size of the give file in the byte
+                Console.WriteLine("The size of the given file is : " + fi.Length.ToString());
+            }
+            catch(FileNotFoundException e)
+            {
+                Console.WriteLine("The given file is not found on the computer : " + e.ToString());
+            }
+        }
+    }
+}
---
+++ HexNumberToDecimal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic; // Import this class to use this class inbuilt methods to convert the hex number to decimal number
+
+/*
+ * Author - Devendra Parmar
+ * Program - Convert a hexadecimal number to decimal number
+ */
+
+namespace HexNumToDecimal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get the hex number from the user
+            Console.Write("Enter the Hexadecimal number : ");
+            string hexNum = Console.ReadLine();
+
+            // Print the hex number
+            Console.WriteLine("This is an Hexadecimal number : " + hexNum);
+
+            // Convert the hex number into decimal number
+            int dec = int.Parse(hexNum, System.Globalization.NumberStyles.HexNumber); // This is the inbuilt method to convert to hex number to decimal
+
+            // Print the converted decimal number
+            Console.WriteLine("The converted hex number into decimal number : " + dec);
+        }
+    }
+}
