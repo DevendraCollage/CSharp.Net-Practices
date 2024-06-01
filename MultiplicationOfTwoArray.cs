3a756c50a898dff8586e3b056fd27f3eb70ca1c7
Devendra Parmar <devendraparmarcollage@gmail.com>
2024-06-01 17:59:07 +0530

    Solve the two questions of the C#.Net 1. Multiply the two array with one another 2. Print the string last character if th length of the string greater 4 otherwise print whole string

---
+++ MultiplicationOfTwoArray.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+/*
+ * Author - Devendra Parmar
+ * Program - Write a C# program to multiply the corresponding elements of two integer arrays.
+ * Note: Use the getlength() method to get and use the length of the array
+ */
+
+namespace MultipleOfTwoArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get the size of the array
+            Console.Write("Enter the size of the array : ");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            // Initialize the two dimensional array
+            int[,] arr = new int[size,size];
+
+            // Get the element of the array from the user
+            Console.WriteLine("\n");
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.Write($"[{i}][{j}] : ");
+                    arr[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            // Initialize the sum array where to store the calculation
+            int[,] sum = new int[size,size];
+
+            // Multiply the array elements
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    sum[i,j] = arr[i,j] * arr[i,j];
+                }
+            }
+
+            // Print the calculation of the two array
+            Console.WriteLine("\nPrint the calculation of the two array : ");
+            for (int i = 0; i < sum.GetLength(0); i++)
+            {
+                for (int j = 0; j < sum.GetLength(1); j++)
+                {
+                    Console.WriteLine(sum[i, j] + " ");
+                }
+            }
+        }
+    }
+}
---
+++ PrintTheStringLast4Characters.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+/*
+ * Author - Devendra Parmar
+ * Program - Write a C# program to create a string of four copies, taking the last four 
+ * characters from a given string. If the given string is less than 4, return the original one.
+ */
+
+namespace PrintTheStringLast4Characters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get the string from the user
+            Console.Write("Enter your string here : ");
+            string str = Console.ReadLine();
+
+            /* Check if the string length is less than 4 then print the original otherwise 
+             * print the last 4 characters of the string 4 four times.
+             * */
+            int len = str.Length; // This will store the string length
+
+            Console.WriteLine("\n");
+            if(len > 4) // if chaacter length greater than 4
+            {
+                string lastFourChar = str.Substring(len - 4, 4); // This will store the last four digit of the string
+                for (int i = 1;  i <= 4;  i++)
+                {
+                    Console.Write(lastFourChar);
+                }
+            }
+            else // Otherwise print whole string
+            {
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
