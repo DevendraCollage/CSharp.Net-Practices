84383336353f4273221efa251bbda57fdb8b817a
Devendra Parmar <devendraparmarcollage@gmail.com>
2024-06-02 15:22:54 +0530

    Solve the two questions of the C#.Net 1. Check the number is multiple of 3 or 7 2. Check the string is starts-with or not

---
+++ CheckTheNumOf3Or7.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+/*
+ * Author - Devendra Parmar
+ * Program -  Write a C# program to check if a given positive number is a multiple of 3 or 7.
+ */
+
+namespace CheckTheNumOf3or7
+{
+    internal class Program
+    {
+        // Function to check the number is multiple of 3 or 7
+        static bool checkNum(int num) // This will check the condition if number is multiple of 3 or 7
+        {
+            if (num % 3==0 || num % 7==0) // If Multiple then return true
+            {
+                return true;
+            }
+            else // Otherwise return false
+            {
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            // Get the number from the user
+            Console.Write("Enter the number here : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            // Call the function and print the result
+            Console.WriteLine(checkNum(num));
+        }
+    }
+}
---
+++ StringStartsWithOrNot.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+/*
+ * Author - Devendra Parmar
+ * Program - Write a C# program to check if a string starts with a specified word.
+ */
+
+namespace StringStartsWith
+{
+    internal class Program
+    {
+        // Function to check string is startswith or not
+        static bool checkStr(string str)
+        {
+            // Check the string is startwith or not
+            if (str.StartsWith("Hello"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            // Get the string from the user
+            Console.Write("Enter your string here : ");
+            string str = Console.ReadLine();
+
+            // Call the function and print the answer
+            Console.WriteLine(checkStr(str));
+        }
+    }
+}
