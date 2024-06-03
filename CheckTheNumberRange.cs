3aa35aa6618a979ad06b8ae20a530589d61a0c13
Devendra Parmar <devendraparmarcollage@gmail.com>
2024-06-03 17:28:13 +0530

    Solve the two questions of the C#.Net 1. Check the Number range between 100 and and greater 2. Check the number range between positive and negative

---
+++ CheckTheNumberRange.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+/*
+ * Author - Devendra Parmar
+ * Program - Write a C# program to check two given numbers where one is less than 100 and 
+ * the other is greater than 200.
+*/
+
+namespace CheckTheNumberRange
+{
+    internal class Program
+    {
+        // Function to check the number is less than 100 and other second number is less than 200
+        static bool checkNum(int num1, int num2)
+        {
+            if(num1 < 100 && num2 > 100)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            // Get the two number from the user
+            Console.Write("Enter the first number (<100) : ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the second number (>100) : ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            // Call the function and print the result
+            Console.WriteLine(checkNum(num1, num2));
+        }
+    }
+}
---
+++ NumberRangeOfPositiveOrNegative.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+/*
+ * Author - Devendra Parmar
+ * Program - Write a C# program to check if an integer (from the two given integers) is in the range -10 to 10.
+ */
+
+namespace NumRange
+{
+    internal class Program
+    {
+        // Function to check the number is between the range of -10 to 10
+        static bool checkNumRange(int num1, int num2)
+        {
+            if(num1 > -10 && num2 < 10)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            // Get the two number from the user
+            Console.Write("Enter the number one here : ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the number two here : ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            // Call the function and to print the result
+            Console.WriteLine(checkNumRange(num1, num2));
+        }
+    }
+}
