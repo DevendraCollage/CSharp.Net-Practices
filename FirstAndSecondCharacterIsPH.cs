054b787fad801d9d7788cdba34a3167c4cc93928
Devendra Parmar <devendraparmarcollage@gmail.com>
2024-06-04 18:34:25 +0530

    Solve the two questions of the C#.Net 1. First and Second Character of the string is HP word then remove this word 2. If String contain HP then remove this characters from the string return other string

---
+++ FirstAndSecondCharacterIsPH.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+/*
+ * Author - Devendra Parmar
+ * Program -  Write a C# program to get a string of two characters from a given string. The first and second characters of the given string must be "P" and "H", so PHP will be "PH".
+ */
+
+namespace FirstAndSecondCharacterPH
+{
+    internal class Program
+    {
+        // Function to check the string is contain P(First) and H(Second) and remove all the characters
+        static string checkStr(string str)
+        {
+            if (str.Length >= 2 && str.Substring(0, 2) == "PH")
+            {
+                return str.Substring(0, 2);
+            }
+            else
+            {
+                return str;
+            }
+        }
+        static void Main(string[] args)
+        {
+            // Get the string from the user
+            Console.Write("Enter the string here : ");
+            string str = Console.ReadLine();
+
+            // Call the function and print the result
+            Console.WriteLine(checkStr(str));
+        }
+    }
+}
---
+++ StringWithSomeHPCharacters.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+/*
+ * Author - Devendra Parmar
+ * Program - Write a C# program to check if "HP" appears at the second position in a string 
+ * and return the string without "HP".
+ */
+
+namespace StringWithSomeHP
+{
+    internal class Program
+    {
+        // Functoion to check the string appearance
+        static string checkStr(String str)
+        {
+            if(str.Substring(1,2).Equals("HP")) // This will check the second postition string is match with HP then remove the HP
+            {
+                return str.Remove(1,2); // This will remove the first and second character of the string if condition will match
+            }
+            else // Otherwise not match then return string
+            {
+                return str; // Othwerise return whole string if condition is not match
+            }
+        }
+        static void Main(string[] args)
+        {
+            // Get the string from the user
+            Console.Write("Enter the string here: ");
+            String str = Console.ReadLine();
+
+            // Call the function and print the result
+            Console.WriteLine(checkStr(str));
+        }
+    }
+}
