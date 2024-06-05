e4e54a6800f719b5496911934389a5e256e1deea
Devendra Parmar <devendraparmarcollage@gmail.com>
2024-06-05 17:00:26 +0530

    Solve the two questions of the C#.Net 1. Find the largest and lowest number from the given three numbers 2. Find the nearest number of 20 from given two numbers

---
+++ FindTheLargestAndLowestFromThreeNumbers.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+/*
+ * Author - Devendra Parmar
+ * Program - Write a C# program to find the largest and lowest values from three integer values.
+ */
+
+namespace FindLargestAndLowestFromThreeNum
+{
+    internal class Program
+    {
+        // Function to check the largest number from the given three nums
+        static void largestNum(double n1, double n2, double n3)
+        {
+            if (n1 > n2 && n2 > n3)
+            {
+                Console.WriteLine(n1 + " Is Greatest Among Them!");
+            }
+            else if (n2 > n3)
+            {
+                Console.WriteLine(n2 + " Is Greatest Among Them!");
+            }
+            else
+            {
+                Console.WriteLine(n3 + " Is Greatest Among Them!");
+            }
+        }
+
+        // Function to check the lowest numbers from the given three nums
+        static void lowestNum(double n1, double n2, double n3)
+        {
+            if (n1 < n2 && n2 < n3)
+            {
+                Console.WriteLine(n1 + " Is Lowest Among Them!");
+            }
+            else if (n2 < n3)
+            {
+                Console.WriteLine(n2 + " Is Lowest Among Them!");
+            }
+            else
+            {
+                Console.WriteLine(n3 + " Is Lowest Among Them!");
+            }
+        }
+        static void Main(string[] args)
+        {
+            // Read the three numbers from the user
+            Console.Write("Enter the number 1 here : ");
+            double num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the number 2 here : ");
+            double num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the number 3 here : ");
+            double num3 = Convert.ToInt32(Console.ReadLine());
+
+            // Call the function for the greatest number among three numbers
+            largestNum(num1, num2, num3);
+
+            // Call the function for the lowest number among three numbers
+            lowestNum(num1, num2, num3);
+        }
+    }
+}
---
+++ FindTheNearestNumberOf20.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+/*
+ * Author - Devendra Parmar
+ * Program - Check the nearest value of 20 of two given integers and return 0 if two numbers are same
+ */
+
+namespace NearestNumOf20
+{
+    internal class Program
+    {
+        // Function to check the nearest number of 20 otherwise return 0
+        static void checkNum(double n1, double n2)
+        {
+            if(n1 == n2)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                if (n1>=15)
+                {
+                    Console.WriteLine(n1 + " Is Nearest Number of 20!");
+                }
+                else if(n2>=15)
+                {
+                    Console.WriteLine(n2 + " Is Nearest Number of 20!");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            // Read the two integers from the user
+            Console.Write("Enter the 1 number here : ");
+            double num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the 2 number here : ");
+            double num2 = Convert.ToInt32(Console.ReadLine());
+
+            // Call the function and pass the numbers
+            checkNum(num1, num2);
+        }
+    }
+}
