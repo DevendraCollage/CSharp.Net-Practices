0617ba31bb1089b90725a9c9316c1b51ce4ac00b
Devendra Parmar <devendraparmarcollage@gmail.com>
2024-06-06 17:33:35 +0530

    Solve the two questions of the C#.Net 1. Count the string characters given by the user 2. Return answer if string contains w character between 1 and 3 otherwise false

---
+++ CountStringCharacters.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+/*
+ * Author - Devendra Parmar
+ * Program - Write a C# program to create a string where the first 4 characters are in lower case. If the string is less than 4 letters, make the whole string in upper case.
+ */
+
+namespace CountStringCharacter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read the string from the user
+            Console.Write("Enter the string here : ");
+            string str = Console.ReadLine();
+
+            // Count the characters of the string
+            int count = str.Count();
+
+            // Make the Condition count > 4 then make first 4 characters in lowercase
+            if(count <= 4)
+            {
+                Console.WriteLine(str.ToUpper());
+            }
+            else
+            {
+                string firstFour = str.Substring(0, 4);
+                string f = firstFour.ToLower();
+                string afterFour = str.Substring(4);
+                string a = afterFour.ToUpper();
+                Console.WriteLine(f + a);
+            }
+        }
+    }
+}
