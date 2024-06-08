b2deabbfbc7a99f798b592962fce9dd449385e7f
Devendra Parmar <devendraparmarcollage@gmail.com>
2024-06-08 17:54:39 +0530

    Solve the two questions of the C#.Net 1. Check the given string by the user is starts with W or not 2. Check String of Odd Characters and print that characters of the given string

---
+++ CheckStringOfOddCharacters.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+/*
+ * Author - Devendra Parmar
+ * Program - Write a C# program to create a string of every other character (odd position) from the first position of a given string.
+ */
+
+namespace CreateStringOfOddCharacters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read the string from the user
+            Console.Write("Enter your string here : ");
+            string str = Console.ReadLine();
+
+            string oddStr = String.Empty; // Initialize empty string
+
+            // Print the string odd character of the given strings
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (i%2==0)
+                {
+                    oddStr = oddStr + str[i];
+                }
+            }
+
+            // Print the string in an odd position
+            Console.WriteLine(oddStr);
+        }
+    }
+}
