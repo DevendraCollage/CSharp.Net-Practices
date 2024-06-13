cd6a571adbdda4f18dfa40e0f779e28ee1165892
Devendra Parmar <devendraparmarcollage@gmail.com>
2024-06-13 17:17:13 +0530

    Solve the two questions of the C#.Net 1. Check the array contains the odd or even number then write true otherwise false 2. Check the given year is Century year or not

---
+++ CheckArrayHaveOddorEvenNum.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+/*
+ * Author - Devendra Parmar
+ * Program - Write a C# program to check if an array contains an odd number.
+ */
+
+namespace CheckArrayHaveOddNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read th size of the array from the user
+            Console.Write("Enter the size of the array : ");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            // Initialize the array and set the size
+            int[] arr = new int[size];
+
+            // Read the elements from the user
+            Console.WriteLine("Enter the elements of the array here : ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("[{0}] : ", i);
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            // Check the array contains odd numbers or not
+            bool flag = true;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 2 == 0)
+                {
+                    flag = false;
+                }
+                else
+                {
+                    flag = true;
+                }
+            }
+
+            // Print the result
+            Console.WriteLine(flag);
+        }
+    }
+}
---
+++ CheckTheGivenCenturyYear.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+/*
+ * Author - Devendra Parmar
+ * Program - Get the century from a year.
+ */
+
+namespace CheckTheCenturyYear
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read the year from the user
+            Console.Write("Enter the year here : ");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            // Check the given year is century year or not
+            if(year%100==0)
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+        }
+    }
+}
