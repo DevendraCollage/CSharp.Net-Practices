68d9fc7767003a8d51ffa025bb03b887d0bcbc5e
Devendra Parmar <devendraparmarcollage@gmail.com>
2024-06-14 16:48:02 +0530

    Solve the two questions of the C#.Net 1. Find the largest product of the array given by the user 2. Check the given string is Palindrome or not

---
+++ LargestProductOfArray.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+/*
+ * Author - Devendra Parmar
+ * Program - Write a C# program to find the pair of adjacent elements that has the largest product of the given array.
+ */
+
+namespace LargestProductOfArray
+{
+    internal class Program
+    {
+        public static int array_adjacent_elem(int[] input_array)
+        {
+            // Initialize variables
+            int index = 0;
+
+            // Calculate the product of the first two elements
+            int product = input_array[index] * input_array[index + 1];
+            index++;
+
+            // Loop to calculate the max adjacent elements of the array
+            while(index+1 < input_array.Length)
+            {
+                product = ((input_array[index] * input_array[index + 1]) > product) ? (input_array[index] * input_array[index+1]) : product;
+                index++;
+            }
+
+            return product;
+        }
+        static void Main(string[] args)
+        {
+            // Read the size of the array
+            Console.Write("Enter the size of the array : ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            // Initialize array and set the size
+            int[] arr = new int[size];
+
+            // Read the elements of the array from the user
+            Console.WriteLine("Enter array elements here : ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("[{0}] : ", i);
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine();
+
+            // Call the function and pass the array
+            Console.WriteLine(array_adjacent_elem(arr));
+        }
+    }
+}
---
+++ PalindromeString.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+/*
+ * Author - Devendra Parmar
+ * Program - Write a C# program to check if a given string is a palindrome or not.
+ */
+
+namespace PalindromeStr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read the string from the user
+            Console.Write("Enter your string here : ");
+            string str = Console.ReadLine();
+
+            string rev = "";
+
+            // Reverse the string
+            for (int i = str.Length -1; i >= 0; i--)
+            {
+                rev = rev + str[i];
+            }
+
+            // Compare the string if paindrome or not
+            if(str == rev)
+            {
+                Console.Write(true);
+            }
+            else
+            {
+                Console.Write(false);
+            }
+        }
+    }
+}
