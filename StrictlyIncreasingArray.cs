c3dbd69564ad97bab00be7a09cbeeea77d0c77ca
Devendra Parmar <devendraparmarcollage@gmail.com>
2024-06-16 12:15:03 +0530

    Solve the two questions of C#.Net 1. Check the given array by the user is strictly increasingly array of not true otherwise false 2. Sum of the given 2D array by the user and print the non zero elements sum to the console

---
+++ StrictlyIncreasingArray.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+/*
+ * Author - Devendra Parmar
+ * Program - Write a C# program to check whether it is possible to create a strictly increasing sequence
+ * from a given sequence of integers as an array.
+ */
+
+namespace StrictlyIncreasingArray
+{
+    internal class Program
+    {
+        // Function to check it is possible or not to create a strictly increasing sequence of array.
+        public static bool test_increasing_sequence(int[] arr)
+        {
+            // Initialize variables
+            int x = 0;
+
+            // Loop through the array and check the elements
+            for (int i = 0; i < arr.Length-1; i++)
+            {
+                // First condition to check 
+                if (arr[i] >= arr[i+1])
+                {
+                    x++;
+                }
+
+                // Second condition if first condition will be false
+                if(i+2 < arr.Length && arr[i] >= arr[i+2])
+                {
+                    x++;    
+                }
+            }
+
+            return x <= 2;
+        }
+        static void Main(string[] args)
+        {
+            // Read the size of the array from the user
+            Console.Write("Enter the size here : ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            // Initialize the array and set the size
+            int[] arr = new int[size];
+
+            // Read the elements of the array from the user
+            Console.WriteLine("Enter the array elements here : ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("[{0}] : ", i);
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            // Call the function and print the result
+            Console.WriteLine(test_increasing_sequence(arr));
+        }
+    }
+}
---
+++ SumOf2X2NonZeroElements.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+/*
+ * Author - Devendra Parmar
+ * Program - Write a C# program to calculate the sum of all integers in a rectangular matrix. However, exclude those integers located below an integer of value 0.
+ * Sample Example:
+ * 
+    matrix = 
+    [[0, 2, 3, 2],
+    [0, 6, 0, 1],
+    [4, 0, 3, 0]]
+
+    Eligible integers which will be participated to calculate the sum -
+    matrix = 
+    [[X, 2, 3, 2],
+    [X, 6, X, 1],
+    [X, X, X, X]]
+
+    Therefore sum will be: 2 + 3 + 2 + 6 + 1 = 14
+ */
+
+namespace _2x2MatrixSum
+{
+    internal class Program
+    {
+        public static int sum_matrix_elements(int[,] arr)
+        {
+            // Variable to store the sum of the non zero elements of the given array
+            int x = 0;
+
+            // Loop through the array elements
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(0) && arr[j,i] >= 0; j++)
+                {
+                    x = x + arr[j,i];
+                }
+            }
+
+            // Return the value of the calculated sum
+            return x;
+        }
+        static void Main(string[] args)
+        {
+            // Read the size of the 2x2 matrix from the user
+            Console.Write("Enter the size of the array : ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            // Initialize the 2x2 matrix and set the size
+            int[,] arr = new int[size,size];
+
+            // Read the elements from the user
+            Console.WriteLine("Enter the array elements below : ");
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(0); j++)
+                {
+                    Console.Write("[{0}][{1}] : ", i, j);
+                    arr[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            // Call the function and print the result
+            Console.WriteLine(sum_matrix_elements(arr));
+
+
+            // Print the 2D array like this given below
+            //    for (int i = 0; i < arr.GetLength(0); i++)
+            //    {
+            //        for (int j = 0; j < arr.GetLength(0); j++)
+            //        {
+            //            Console.WriteLine("[{0}][{1}] : {2}", i, j, arr[i, j]);
+            //        }
+            //    }
+            //}
+        }
+    }
+}
