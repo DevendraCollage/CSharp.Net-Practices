a169af2e87b301ce550ca09199a7eb7a53e12d3a
Devendra Parmar <devendraparmarcollage@gmail.com>
2024-06-22 19:36:32 +0530

    Solve the two questions of the C#.net 1. Match given string word with the parenthesis and remove the parenthesis 2. Sort given array without touching -5 in the array

---
+++ MatchTheStringWordWithParenthesis.cs
@@ -0,0 +1,28 @@
+﻿namespace MatchTheStringWordWithParentheses
+{
+    internal class Program
+    {
+        // Function to check the string has an parenthesis or not
+        public static string reverse_remove_parenthese(string str)
+        {
+            int lid = str.LastIndexOf('('); // Get the any parenthesis last over index
+
+            if (lid == -1)
+            {
+                return str;
+            }
+            else
+            {
+                int rid = str.IndexOf(')', lid);
+                return reverse_remove_parenthese(
+                str.Substring(0, lid) +
+                new string(str.Substring(lid + 1, rid - lid - 1).Reverse().ToArray()) +
+                str.Substring(rid + 1));
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(reverse_remove_parenthese("ab(dc)ef"));
+        }
+    }
+}
---
+++ SortArrayWithoutTouching_5Minus.cs
@@ -0,0 +1,24 @@
+﻿namespace SortArrayWithoutTouching_5Minus
+{
+    internal class Program
+    {
+        // Method to sort the number without touching -5
+        public static int[] sort_numbers(int[] arr)
+        {
+            int[] num = arr.Where(x => x != 5).OrderBy(x => x).ToArray();
+            int ctr = 0;
+            return arr.Select(x => x >= 0 ? num[ctr++] : -5).ToArray();
+        }
+        static void Main(string[] args)
+        {
+            // Create the array of unsorted numbers
+            int[] x = sort_numbers(new int[]{ -5, 12, 48, 85, -5, 45, 75, 96, 3 });
+
+            // Now print the sorted array
+            foreach(var i in x)
+            {
+                Console.WriteLine(i.ToString());
+            }
+        }
+    }
+}
