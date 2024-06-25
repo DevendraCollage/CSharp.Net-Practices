73b7d46221a22b610e3fd0b4260c6a1789bd9233
Devendra Parmar <devendraparmarcollage@gmail.com>
2024-06-25 11:07:36 +0530

    Solve the two questions of the .Net 1. Find the given number present in the array 2. Find the filename with their extension of the given

---
+++ FindTheFilenameWithExt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+/*
+ * Author : Devendra Parmar
+ * Question : Write a C# Sharp program to get the file name (including extension) from a given path.
+ */
+
+namespace FindFilenameWithExt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Find the file name extension
+            string filePath = "C:/Users/Devendra/Desktop/C#.Net/Practice Programs/FindFilenameWithExt/Program.cs";
+
+            Console.WriteLine(filePath.Split('/').Last());
+        }
+    }
+}
