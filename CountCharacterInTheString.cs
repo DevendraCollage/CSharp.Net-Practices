85c1f7d5c354d85668e9547182134f3ca22db6bc
Devendra Parmar <devendraparmarcollage@gmail.com>
2024-06-27 17:50:04 +0530

    Solve the two questions of the .Net 1. Count the given character in the string given by the user 2. Replace string character with the number define in the program

---
+++ CountCharacterInTheString.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+/*
+ * Author : Devendra Parmar
+ * Question : Write a C# Sharp program to count a specified character (both cases) in a given string.
+ */
+
+namespace CountCharacterintheString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the String here : ");
+            string str = Console.ReadLine()!;
+            Console.WriteLine();
+
+            Console.Write("Enter the string character to find how many time occur : ");
+            char wordChar = Console.ReadKey().KeyChar!;
+            Console.WriteLine();
+
+            int count = 0;
+
+            foreach (char item in str)
+            {
+                if(char.ToLower(item) == wordChar)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine("\nThe given word {0} character count is : {1}", wordChar, count);
+        }
+    }
+}
---
+++ ReplaceStringCharactersWithNumbers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+/*
+ * Author : Devendra Parmar
+ * Question : Write a C# Sharp program to create a coded string from a given string, using a specified formula.
+ * Replace all 'P' with '9', 'T' with '0', 'S' with '1', 'H' with '6' and 'A' with '8'.
+ */
+
+namespace ReplaceStringCharacterWithNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the string word here : ");
+            string word = Console.ReadLine()!;
+
+            string modified = word.Replace("P", "9").Replace("T", "0").Replace("S", "1").Replace("H", "6").Replace("A", "8");
+
+            Console.WriteLine("\nThe Modified String : {0}", modified);
+        }
+    }
+}
