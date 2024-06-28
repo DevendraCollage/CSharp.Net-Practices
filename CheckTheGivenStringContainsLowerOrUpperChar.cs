46d220151e8fa9f276874c3ae764510ffb7d0cef
Devendra Parmar <23030501043@darshan.ac.in>
2024-06-28 17:20:13 +0530

    Solve the two questions of the .Net 1. Check the given string by the user contains the lowercase and uppercase letters 2. Remove the first and last character from the string

---
+++ CheckTheGivenStringContainsLowerOrUpperChar.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+/*
+ * Author : Devendra Parmar
+ * Question : Write a C# Sharp program to check if a given string contains only lowercase
+ * or uppercase characters.
+ */
+
+namespace CheckTheGivenStringIsContainsLowercaseorUppercase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the string here : ");
+            string str = Console.ReadLine()!;
+
+            string lower = str.ToLower();
+            string upper = str.ToUpper();
+
+            // Check given string contains lowercase or uppercase characters
+            if(str == lower || str == upper)
+            {
+                Console.Write(true);
+            }
+            else
+            {
+                Console.Write(false);
+            }
+        }
+    }
+}
---
+++ RemoveTheFirstAndLastCharacterOfTheString.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+/*
+ * Author : Devendra Parmar
+ * Question : Write a C# Sharp program to remove the first and last elements from a given string.
+ * Sample Output:
+ * -------------------------------
+ * Original string: PHP
+ * After removing first and last elements: H
+ * Original string: Python
+ * After removing first and last elements: ytho
+ * Original string: JavaScript
+ * After removing first and last elements: avaScrip
+ */
+
+namespace RemoveTheFirstAndLastFromTheString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your string here : ");
+            string str = Console.ReadLine()!;
+
+            // Get the first and last character
+            Console.WriteLine(str.Substring(1, str.Length - 2));
+        }
+    }
+}
