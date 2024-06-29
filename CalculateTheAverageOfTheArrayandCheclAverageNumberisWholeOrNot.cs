3fb181a26f5dc2dc380f18fb4bcf46c1f2b3b47c
Devendra Parmar <23030501043@darshan.ac.in>
2024-06-29 10:34:22 +0530

    Solve the two questions of the .net 1. Calculate the average of the given array by the user and check the average is whole number or not 2. Convert the given string in the alphabetical order in the same case

---
+++ CalculateTheAverageOfTheArrayandCheclAverageNumberisWholeOrNot.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+/*
+ * Author : Devendra Parmar
+ * Question : Write a C# Sharp program to check whether the average value of the elements
+ * of a given array of numbers is a whole number or not.
+ */
+
+namespace CalculateTheAverageOfTheArrayAndCheckTheNumberIsWhole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the size of the Array : ");
+            int size = int.Parse(Console.ReadLine()!);
+            Console.WriteLine();
+
+            int[] arr = new int[size];
+
+            Console.WriteLine("Enter the array element below : ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("[{0}] : ", i);
+                arr[i] = int.Parse(Console.ReadLine()!);
+            }
+            Console.WriteLine();
+
+            double average = arr.Average();
+            Console.WriteLine("Your Array Average is : {0}", average);
+            Console.WriteLine();
+
+            if(average % 1 == 0)
+            {
+                Console.Write("Which is Whole Number : ");
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.Write("Which is Not Whole Number : ");
+                Console.WriteLine(false);
+            }
+        }
+    }
+}
---
+++ ConvertTheStringInAlphabaticalOrder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/*
+ * Author : Devendra Parmar
+ * Question : Write a C# Sharp program to convert the letters of a given string (same case-upper/lower)
+ * into alphabetical order.
+ */
+
+namespace ConvertTheStringinAlphabaticallyOrder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the string here : ");
+            string str = Console.ReadLine()!;
+            Console.WriteLine();
+
+            // Convert to character array
+            char[] charArray = str.ToArray();
+            // And after that sort the character array
+            Array.Sort(charArray);
+
+            Console.WriteLine(charArray);
+        }
+    }
+}
