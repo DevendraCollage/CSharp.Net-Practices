4b391816be662161dd25edf1eea2ecc04c257b01
Devendra Parmar <23030501043@darshan.ac.in>
2024-06-30 11:52:00 +0530

    Solve the two questions of the dotnet 1. Convert the given string to integer and vice versa 2. Convert the mixed value array to an string

---
+++ ConvertMixedValueofArraytoString.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+/*
+ * Author : Devendra Parmar
+ * Question : Write a C# Sharp program to convert all the values of a given array of mixed values to string values.
+ */
+
+namespace ConvertMixedValueOfArraytoString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the size of the Array : ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            object[] arr = new object[size];
+
+            Console.WriteLine("Enter the array elements : ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("[{0}] : ", i);
+                arr[i] = Console.ReadLine()!;
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Print the value and their type");
+            string[] strArr = new string[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                strArr[i] = arr[i].ToString()!;
+                Console.WriteLine("[{0}] : {1} + and type is {2}", i, strArr[i], strArr[i].GetType());
+            }
+        }
+    }
+}
---
+++ ConvertStringToIntegerAndViceversa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+/*
+ * Author : Devendra Parmar
+ * Question : Write a C# Sharp program to convert an integer to a string and a string to an integer.
+ */
+
+namespace ConvertStringtoIntegerAndViceVersa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number here : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            string strNum = Convert.ToString(num);
+
+            Console.WriteLine("The Number Convert Into String : {0} and Their Type : {1}", strNum, strNum.GetType());
+            Console.WriteLine("The String Number Convert Into Number : {0} and Their Type : {1}", Convert.ToInt32(strNum), Convert.ToInt32(strNum).GetType());
+
+            Console.WriteLine("\nThe String Number Convert Into Number : {0} and Their Type : {1}", num, num.GetType());
+            Console.WriteLine("The Number Convert Into String : {0} and Their Type : {1}", strNum, strNum.GetType());
+        }
+    }
+}
