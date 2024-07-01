c143cf05a8a792c5490fd03060e0d67cdf4c29fc
Devendra Parmar <23030501043@darshan.ac.in>
2024-07-01 17:50:18 +0530

    Solved the two questions of the DotNet 1. Print the nth odd number of the user input 2. Print the ascci code of the given character by the user

---
+++ PrintNthOddNumberofTheGivenNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/*
+ * Author : Devendra Parmar
+ * Question : Write a C# Sharp program that takes a positive number and returns the nth odd number.
+ */
+
+namespace PrintGivenNumbernthOddNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the range you want to print odd numbers : ");
+            int range = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("-- Print the odd numbers of the nth --");
+            int odd = 1;
+            for (int i = 1; i <= range; i++)
+            {
+                Console.WriteLine("{0}th odd number : {1}", i, odd);
+                odd += 2;
+            }
+        }
+    }
+}
---
+++ PrintTheAscciCodeofTheGivenCharacter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+/*
+ * Author : Devendra Parmar
+ * Question : Write a C# Sharp program to get the ASCII value of a given character.
+ */
+
+namespace AscciCodeOfTheGivenNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number here : ");
+            char chr = Console.ReadKey().KeyChar!;
+            Console.WriteLine();
+
+            Console.WriteLine("The Ascci Code {0} is {1}", chr, (int)chr);
+        }
+    }
+}
