3c45891279977018fa1597dc96b6f62ff5e9eefd
Devendra Parmar <23030501043@darshan.ac.in>
2024-07-02 18:40:26 +0530

    Solve the two questions of the .Net 1. Check the given word is Plural or not 2. Find the Square sum of the Array Elements

---
+++ CheckTheGivenWordIsPluralOrNot.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+/*
+ * Author : Devendra Parmar
+ * Question : Write a C# Sharp program to check whether a word is plural or not.
+ */
+
+namespace CheckGivenWordIsPluralOrNot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the word here : ");
+            string word = Console.ReadLine()!;
+
+            if (word.EndsWith('s'))
+            {
+                Console.WriteLine("This is a Plural Word!");
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine("This is not Plural Word!");
+                Console.WriteLine(false);
+            }
+        }
+    }
+}
---
+++ FindTheSquareOfTheArrayElements.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+/*
+ * Author : Devendra Parmar
+ * Question : Write a C# Sharp program to find the sum of squares of elements in a given array of integers.
+ */
+
+namespace FIndTheSumOfTheSquareOfTheArrayElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the size of the Array : ");
+            int size = Convert.ToInt32(Console.ReadLine()!);
+
+            int[] arr = new int[size];
+            int sum = 0;
+
+            Console.WriteLine("\nEnter the Array Elements : ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("[{0}] : ", i);
+                arr[i] = Convert.ToInt32(Console.ReadLine()!);
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = arr[i] * arr[i];
+                sum = sum + arr[i];
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
