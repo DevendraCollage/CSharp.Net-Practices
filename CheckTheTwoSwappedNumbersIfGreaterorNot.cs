9874dfa4c92ff3b909432f2bbcc925e53212ca8a
Devendra Parmar <23030501043@darshan.ac.in>
2024-07-03 17:45:28 +0530

    Solve two questions of the .Net 1.  Check the two given integers are same after swapped 2. Remove the non letter characters from the string and return the string

---
+++ CheckTheTwoSwappedNumbersIfGreaterorNot.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+/*
+ * Author : Devendra Parmar
+ * Question : Write a C# Sharp program to swap a two-digit number
+ * and check whether the given number is greater than its swap value.
+ */
+
+namespace CheckTheSwappedNumberofTheTwoNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number 1 : ");
+            int num1 = Convert.ToInt32(Console.ReadLine()!);
+            Console.Write("Enter the number 1 : ");
+            int num2 = Convert.ToInt32(Console.ReadLine()!);
+            Console.WriteLine();
+
+            Console.WriteLine("Before Swapping These Numbers : ");
+            Console.WriteLine("Number-1 : {0}", num1);
+            Console.WriteLine("Number-2 : {0}", num2);
+            Console.WriteLine();
+
+            int digit = num1;
+            num1 = num2;
+            num2 = digit;
+
+            Console.WriteLine("After Swapping These Numbers : ");
+            Console.WriteLine("Number-1 : {0}", num1);
+            Console.WriteLine("Number-2 : {0}", digit);
+            Console.WriteLine();
+
+            if (num1 > digit)
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+        }
+    }
+}
---
+++ RremoveNonLetterCharacters.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+/*
+ * Author : Devendra Parmar
+ * Question : Write a C# Sharp program to remove all non-letter
+ * characters from a given string. From Wikipedia, A letter is a
+ * segmental symbol of a phonemic writing system. 
+ */
+
+namespace RemoveNonLetterCharacters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the string here : ");
+            string obj = Console.ReadLine()!;
+
+        Console.WriteLine(Regex.Replace(obj, @"[^a-zA-Z]", ""));
+        }
+    }
+}
