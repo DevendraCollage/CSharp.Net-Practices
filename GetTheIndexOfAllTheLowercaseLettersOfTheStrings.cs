bd119eb2589d79c5fc1909d16c378422a06fe14e
Devendra Parmar <23030501043@darshan.ac.in>
2024-07-04 18:01:20 +0530

    Solve the two questions of the DotNet 1. Get the index of the lowercase letters of the given string 2. Remove vowels from the given strings by the user

---
+++ GetTheIndexOfAllTheLowercaseLettersOfTheStrings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+/*
+ * Author : Devendra Parmar
+ * Question : Write a C# Sharp program to get the index number of all lower case letters in a given string.
+ */
+
+namespace GetTheIndexOfAllTheLowercaseLetters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the string here : ");
+            string str = Console.ReadLine()!;
+            Console.WriteLine();
+
+            char[] chars = str.ToCharArray();
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (char.IsLower(chars[i]))
+                {
+                    Console.Write(i + ", ");
+                }
+            }
+        }
+    }
+}
---
+++ RemoveVowelsFromTheGivenString.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+/*
+ * Author : Devendra Parmar
+ * Question : Write a C# Sharp program to remove all vowels from a given string.
+ */
+
+namespace RemoveVowelsFromTheGivenString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the string here : ");
+            string str = Console.ReadLine()!;
+
+            char[] array = str.ToCharArray();
+            string result = "";
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] != 'a' && array[i] != 'i' && array[i] != 'e' && array[i] != 'o' && array[i] != 'u')
+                {
+                    result = result + array[i];
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
