c8f98ac3d4dcedfe627c12ad1c8c8580e20dbb74
Devendra Parmar <23030501043@darshan.ac.in>
2024-07-05 17:22:25 +0530

    Solve the two question of the DotNet 1. Calculate the cumulative array sum one by one with every element 2. Calculate the count of the character and digit

---
+++ CalculateTheCharacterAndDigitsinTheString.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data.SqlTypes;
+
+/*
+ * Author : Devendra Parmar
+ * Question : Write a C# Sharp program to get the number of letters and digits in a given string.
+ */
+
+namespace CalculateTheCharAndNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the string here : ");
+            string str = Console.ReadLine()!;
+
+            int strCount = 0;
+            int digitCount = 0;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsLetter(str[i]))
+                {
+                    strCount++;
+                }
+                else
+                {
+                    digitCount++;
+                }
+            }
+
+            Console.WriteLine("The Character Count of the String is : {0}", strCount);
+            Console.WriteLine("The Count of the digits in the String is : {0} ", digitCount); 
+        }
+    }
+}
---
+++ CumulativeArrayOfSum.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+/*
+ * Author : Devendra Parmar
+ * Question : Write a C# Sharp program to find the cumulative sum of an array of numbers.
+ * A cumulative sum is a sequence of partial sums of a given sequence. 
+ * For example, the cumulative sums of the sequence {x, y, z,...}, are x , x+y , x+y+z
+ */
+
+namespace CumulativeSumOfTheArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the size of the array : ");
+            int size = Convert.ToInt32(Console.ReadLine()!);
+            Console.WriteLine();
+
+            int[] arr = new int[size];
+
+            Console.WriteLine("Enter the array elements below : ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("[{0}] : ", i);
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine();
+
+            int[] sumArr = new int[size];
+            sumArr[0] = arr[0];
+
+            Console.WriteLine(sumArr[0]);
+            for (int i = 1; i < sumArr.Length; i++)
+            {
+                sumArr[i] = arr[i-1] + arr[i];
+                Console.WriteLine(sumArr[i]);
+            }
+        }
+    }
+}
