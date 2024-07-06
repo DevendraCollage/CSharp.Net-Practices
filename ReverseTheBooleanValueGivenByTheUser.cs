73b2170d321aa28610305b9d060dd94e1a39877c
Devendra Parmar <23030501043@darshan.ac.in>
2024-07-06 09:28:54 +0530

    Solve the two questions of the DotNet 1. Reverse The Boolean Value given by the user 2. Sum of the Polygon angles given by the user

---
+++ ReverseTheBooleanValueGivenByTheUser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+/*
+ * Author : Devendra Parmar
+ * Question : Write a C# Sharp program to reverse a Boolean value.
+ */
+
+namespace ReverseTheBooleanValue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the boolean value in integer : ");
+            string value = Console.ReadLine()!;
+
+            if (value == "True" || value == "true")
+            {
+                Console.WriteLine("False");
+            }else if (value == "False" || value == "false")
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input");               
+            }
+        }
+    }
+}
---
+++ SumOfThePolygonAnglesGivenByTheUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+/*
+ * Author : Devendra Parmar
+ * Question : Write a C# Sharp program to find the sum of the
+ * interior angles (in degrees) of a given polygon. Input the number of straight lines.
+ */
+
+namespace SumofThePolygonAngles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the angle of the polygon : ");
+            double angle = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            double area = (angle - 2) * 180;
+
+            Console.WriteLine("The Sum of {0} : {1}", angle, area);
+        }
+    }
+}
