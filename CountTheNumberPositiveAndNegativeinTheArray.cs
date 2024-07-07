68b9ce0257d53fdeac7bc8d6e1aea0c1bbbe8462
Devendra Parmar <23030501043@darshan.ac.in>
2024-07-07 08:17:59 +0530

    Solve the two questions of the DotNet 1. Count the positive and negative numbers in the given array 2. Count the ones and zeros in the binary number of the given number

---
+++ CountTheNumberPositiveAndNegativeinTheArray.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+/*
+ * Author : Devendra Parmar
+ * Question : Write a C# Sharp program to count positive and
+ * negative numbers in a given array of integers.
+ */
+
+namespace CountPositiveandNegativeNumberinArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the size of the Array : ");
+            int size = Convert.ToInt32(Console.ReadLine()!);
+            Console.WriteLine();
+
+            int[] arr = new int[size];
+
+            Console.WriteLine("Enter the element here : ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("[{0}] : ", i);
+                arr[i] = Convert.ToInt32(Console.ReadLine()!);
+            }
+
+            int posNumber = 0;
+            int negativeNum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > 0)
+                {
+                    posNumber++;
+                }
+                else
+                {
+                    negativeNum++;
+                }
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("The Positive Number in the Array : {0}", posNumber);
+            Console.WriteLine("The Negative Number in the Array : {0}", negativeNum);
+        }
+    }
+}
---
+++ CountTheOnesAndZerosInTheBinaryNumber.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+/*
+ * Author : Devendra Parmar
+ * Question : Write a C# Sharp program to count the number of
+ * ones and zeros in the binary representation of a given integer.
+ */
+
+namespace CountTheOnesAndZerosInTheBinary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number here : ");
+            int num = Convert.ToInt32(Console.ReadLine()!);
+
+            string binaryNum = Convert.ToString(num, 2);
+            char[] arr = binaryNum.ToCharArray();
+
+            int zerosCount = 0;
+            int onesCount = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {    
+                if (arr[i] == '0')
+                {
+                    zerosCount++;
+                }
+                else
+                {
+                    onesCount++;
+                }
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("The Zeros Count in the Binary Number is : {0}", zerosCount);
+            Console.WriteLine("The Ones Count in the Binary Number is : {0}", onesCount);
+        }
+    }
+}
