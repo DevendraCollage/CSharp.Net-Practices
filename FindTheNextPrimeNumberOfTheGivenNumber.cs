74a8cb1db36f71f16c5e716c0211646bbf22207b
Devendra Parmar <23030501043@darshan.ac.in>
2024-07-08 19:34:52 +0530

    Solve the two questions of the DotNet 1. Remove the other values from the array given by the user without touching the integer values 2. Find the next prime number of the given number

---
+++ FindTheNextPrimeNumberOfTheGivenNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+/*
+ * Author : Devendra Parmar
+ * Question : Write a C# Sharp program to find the next prime number of a given
+ * number. If the given number is a prime number, return the number.
+ */
+
+namespace FindTheNextPrimeNumberOfTheGivenPrimeNum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the prime number here : ");
+            int prime = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 2; i < prime; i++)
+            {
+                if (prime%i==0)
+                {
+                    prime++;
+                    i = 2;
+                }
+            }
+
+            Console.WriteLine(prime);
+        }
+    }
+}
---
+++ RemoveOtherValuesFromTheArrayWithoutInteger.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+/*
+ * Author : Devendra Parmar
+ * Question : Write a C# Sharp program to remove all values except integer values from a given array of mixed values.
+ */
+
+namespace RemoveOtherValuesfromArrayWithoutTouchInteger
+{
+    internal class Program
+    {
+
+        public static int[] test(object[] nums)
+        {
+            return nums.OfType<int>().ToArray();
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the size of the array : ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            object[] arr = new object[size];
+
+            Console.WriteLine("Enter array elements here : ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("[{0}] : ", i);
+                string input = Console.ReadLine()!;
+
+                if (int.TryParse(input, out int intValue))
+                {
+                    arr[i] = intValue;
+                }
+                else
+                {
+                    arr[i] = input;
+                }
+            }
+
+            int[] numArr = test(arr);
+
+            Console.WriteLine("\nAfter removing all the values except integer values from the said array of mixed values:");
+            for (int i = 0; i < numArr.Length; i++)
+            {
+                Console.WriteLine(numArr[i] + " ");
+            }
+        }
+    }
+}
