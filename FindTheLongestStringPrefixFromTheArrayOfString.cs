0cba2d618a1a59ec1dac20262eceb58a50df38eb
Devendra Parmar <23030501043@darshan.ac.in>
2024-07-10 19:23:06 +0530

    Solve the two questions of the DotNet 1. Find the square root of the given number by the user 2. Find the longest prefix of the string given array

---
+++ FindTheLongestStringPrefixFromTheArrayOfString.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+/*
+ * Author : Devendra Parmar
+ * Question : Write a C# program that finds the longest common prefix from an array of strings.
+ */
+
+namespace FindTheLongestCommonPrefixFromTheStringArray
+{
+    internal class Program
+    {
+        public static string test(string[] arrString)
+        {
+            if (arrString.Length == 0 || Array.IndexOf(arrString, "") != -1)
+            {
+                return "";
+            }
+            string result = arrString[0];
+            int len = arrString.Length;
+
+            foreach(string word in arrString)
+            {
+                int j = 0;
+
+                foreach (char c in word)
+                {
+                    if (j >= len || result[j] != c)
+                    {
+                        break;
+                    }
+                    j++;
+                }
+
+                len = Math.Min(len, j);
+            }
+
+            return result.Substring(0, len);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the size of the array : ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            string[] arr = new string[size];
+
+            Console.WriteLine("Enter the array Elements here : ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("[{0}] : ", i);
+                arr[i] = Console.ReadLine()!;
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("The Longest String Prefix is : {0}",test(arr));
+        }
+    }
+}
---
+++ FindTheSquareRootOfTheGivenNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+/*
+ * Author : Devendra Parmar
+ * Question : Write a C# Sharp program to calculate the square root of a given 
+ * number. Return the integer part of the result.
+ */
+
+namespace SquareRootOfTheGivenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number here : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The Sqaure root of {0} => {1}", num, Math.Sqrt(num));
+        }
+    }
+}
