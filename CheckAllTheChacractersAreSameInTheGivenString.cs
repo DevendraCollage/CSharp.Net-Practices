cf184c9c2bf1f198db5b158531b99a78f5fb71ea
Devendra Parmar <23030501043@darshan.ac.in>
2024-07-14 10:27:41 +0530

    Solve two questions of the DotNet 1. Check all the characters are same in the given string or not then return false 2. Check the string contains proper end brackets or not otherwise return false

---
+++ CheckAllTheChacractersAreSameInTheGivenString.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+/*
+ * Author : Devendra Parmar
+ * Question : Write a C# Sharp program to check whether all characters in a
+ * string are the same. Return true if all the characters in the string are
+ * the same, otherwise false.
+ */
+
+namespace CheckTheAllChacractersInTheStringAreSame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the string here : ");
+            string str = Console.ReadLine()!;
+            Console.WriteLine();
+
+            char[] chars = str.ToCharArray();
+
+            bool flag = true;
+
+            var c = chars[0];
+
+            for (int i = 1; i < chars.Length; i++)
+            {
+                var d = chars[i];
+
+                if (c != d)
+                {
+                    flag = false;
+                }
+            }
+
+            Console.WriteLine(flag);
+        }
+    }
+}
---
+++ CheckTheStringContainsProperBracketsOrNot.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+/*
+ * Author : Devendra Parmar
+ * Question : A given string contains the bracket characters '(', ')', '{', '}',
+ * '<', ‘>', '[' and ']',
+ * Write a C# programme to check the said string is valid or not. The input 
+ * string will be valid when open brackets and closed brackets are same type 
+ * of brackets.
+ * Or
+ * open brackets will be closed in proper order.
+ */
+
+namespace CheckTheStringContainsTheProperBrackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the string here : ");
+            string brack = Console.ReadLine()!;
+
+            Stack<char> ch = new Stack<char>();
+
+            foreach (var item in brack.ToCharArray())
+            {
+                if (item == '(')
+                {
+                    ch.Push(')');
+                }else if(item == '<')
+                {
+                    ch.Push('>');
+                }
+                else if (item == '[')
+                {
+                    ch.Push(']');
+                }
+                else if (item == '{')
+                {
+                    ch.Push('}');
+                }
+                else if(ch.Count == 0 || ch.Pop() != item)
+                {
+                    Console.WriteLine(false);
+                }
+            }
+                
+            Console.WriteLine(ch.Count == 0 ? true :  false);
+        }
+    }
+}
