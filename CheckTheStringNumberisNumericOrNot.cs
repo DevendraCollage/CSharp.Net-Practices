63dea0a1869c781e1e243f94f888d89f87c223a9
Devendra Parmar <23030501043@darshan.ac.in>
2024-07-17 17:57:01 +0530

    Solve the two questions of the DotNet 1. Check the string number is numeric or not given by the user 2. Print the prime numbers in the strictly descending order

---
+++ CheckTheStringNumberisNumericOrNot.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+/*
+ * Author : Devendra Parmar
+ * Question : Write a C# Sharp program to check if a given string 
+ * (floating point and negative numbers included) is numeric or not. 
+ * Return True if the string is numeric, otherwise false.
+ */
+
+namespace CheckTheStringNumberIsNumericOrNot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter You String Here : ");
+            string useStr = Console.ReadLine()!;
+
+            double result;
+            bool flag = false;
+            if (double.TryParse(useStr, out result))
+            {
+                flag = true;
+            }
+
+            Console.WriteLine("\nThe Original Result is : {0}", result);
+            Console.WriteLine("The Given String Is Numeric Or Not : {0}",flag);
+            
+        }
+    }
+}
---
+++ PrintPrimeNumbersInStrictlyDescendingOrder.cs
@@ -0,0 +1,73 @@
+﻿namespace PrintPrimeNumbersInStrictlyDescendingOrder
+{
+    internal class Program
+    {
+        // Function to chcek the number is prime or not
+        public static bool isPrime(uint n)
+        {
+            if (n < 1) 
+            {
+                return false;
+            }
+
+            bool flag = false;
+            for (int i = 2; i <= n / 2; i++)
+            {
+                if (n % i == 0)
+                {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if (flag)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        static void Main(string[] args)
+        {
+            //Console.Write("Enter the Number here : ");
+            //int num = Convert.ToInt32(Console.ReadLine());
+
+            uint z = 0; 
+            int nc;
+
+            var p = new uint[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var nxt = new uint[128];
+
+            while (true)
+            {
+                nc = 0; 
+
+                foreach (var x in p)
+                {
+                    if (isPrime(x))
+                    {
+                        Console.Write("{0,8}{1}", x, ++z % 5 == 0 ? "\n" : " ");
+                    }
+
+                    for (uint y = x * 10, l = x % 10 + y++; y < l; y++)
+                        nxt[nc++] = y;
+                }
+
+                if (nc > 1)
+                {
+                    Array.Resize(ref p, nc);
+                    Array.Copy(nxt, p, nc);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            // Output the number of descending primes found
+            Console.WriteLine("\n{0} descending primes found", z);
+        }
+    }
+}
